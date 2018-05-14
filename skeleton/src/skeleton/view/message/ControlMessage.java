fe786a45f3fb0a57d3620b3f2865ecf612a89850
Daniel Lakatos <lakatos.dani96@gmail.com>
2018-05-14 07:14:06 +0200

    fixed typo

--- skeleton/src/skeleton/view/message/ControlMessage.java
+++ skeleton/src/skeleton/view/message/ControlMessage.java
@@ -1,7 +1,7 @@
 package skeleton.view.message;
 
 public abstract class ControlMessage {
-	public final ControlMessageType type		//A kontroll üzenet konkrét típusa
+	public final ControlMessageType type;		//A kontroll üzenet konkrét típusa
 	
 	/**
 	 * Az irányítás üzenetek õse, a munkásokat utasítja
